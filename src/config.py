c95a016e0857bff71bf7f71780f5b0bfd8588db0
peterslapbass <103006028+peterslapbass@users.noreply.github.com>
2026-05-03 19:44:55 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -2,8 +2,13 @@ import os
 
 SINCA_URL = "https://sinca.mma.gob.cl/index.php/json/listadomapa2k19"
 
-PM25_WARNING = 50
-PM25_CRITICAL = 100
+THRESHOLDS = {
+    "PM25": 50,
+    "PM10": 100,
+    "NO2": 200,
+    "SO2": 125,
+    "CO": 10
+}
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
